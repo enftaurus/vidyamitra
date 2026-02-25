3502202bb2150fd8c574e832e2730025da2b9eeb
enftaurus <chowdhary.raja0607@gmail.com>
2026-02-25 19:14:21 +0530

    bug fix

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 load_dotenv()
 from services.db_client import supabase
@@ -6,6 +7,7 @@ from services.db_client import supabase
 from routes import login,register,logout,resume_upload
 
 app=FastAPI()
+app.add_middleware( CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"])
 app.include_router(login.router)
 app.include_router(register.router)
 app.include_router(logout.router)
